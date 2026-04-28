d4e2bdffe6bf3897e5a8d1129e2047f9a8143de4
Advait Palve <advaitdpalve@gmail.com>
2026-04-28 16:31:04 +0530

    feat: widgets barrel export (core library index)

---
+++ fuel_cred/lib/core/widgets/widgets.dart
@@ -0,0 +1,20 @@
+/// Core Widgets Library Export
+/// All reusable UI components in one place
+
+// Buttons
+export 'fuelpay_button.dart';
+
+// Cards
+export 'cards.dart';
+
+// App Bars
+export 'app_bars.dart';
+
+// Text Fields
+export 'text_fields.dart';
+
+// Gamification Components
+export 'gamification.dart';
+
+// Shared Components
+export 'shared_components.dart';
